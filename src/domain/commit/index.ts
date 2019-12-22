22206ca45e466ac425152c570441a2ad8859a540
Nguyen Anh Tu <tunguyen.bhtech@gmail.com>
2019-12-22 21:04:08 +0700

    feat(domain): commit - github user and commit domain

---
+++ src/domain/commit/index.ts
@@ -0,0 +1,24 @@
+export interface CommitVerfication {
+    verified: boolean;
+    reason: string;
+    signature: string;
+    payload: string;
+}
+
+export interface CommitAuthor {
+    id: number;
+    avatar_url: string;
+    html_url: string;
+    name: string;
+    email: string;
+    date: string; // ISO date string
+}
+
+export interface Commit {
+    url: string;
+    author: CommitAuthor;
+    committer: CommitAuthor;
+    message: string;
+    comment_count: number;
+    verfication: CommitVerfication;
+}
